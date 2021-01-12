4c2bb84d7d28d60078674a2ca7f045a6d14e033c
Marc Durdin <marc@durdin.net>
2021-01-13 10:36:44 +1100

    fix: use new api for language examples
    Fixes #13.
    
    Language examples are now loaded from api.keyman.com/keyboard
    instead of from r.keymanweb.com. Also added _common/KeymanHosts.php
    and updated language examples to reference that instead of
    hardcoded url.

--- prog/languageexample.php
+++ prog/languageexample.php
@@ -1,18 +1,18 @@
 <?php /*
   Name:             languageexample
   Copyright:        Copyright (C) 2005 Tavultesoft Pty Ltd.
-  Documentation:    
-  Description:      
+  Documentation:
+  Description:
   Create Date:      24 Aug 2009
 
   Modified Date:    20 Aug 2010
   Authors:          mcdurdin, mcdurdin-admin
-  Related Files:    
-  Dependencies:     
+  Related Files:
+  Dependencies:
 
-  Bugs:             
-  Todo:             
-  Notes:            
+  Bugs:
+  Todo:
+  Notes:
   History:          24 Aug 2009 - mcdurdin - Fixup text on examples
                     04 Sep 2009 - mcdurdin - Show example text for no keyboards localised
                     17 Oct 2009 - mcdurdin - Fixup translation of no keyboard text
@@ -21,25 +21,28 @@
                     20 Aug 2010 - mcdurdin-admin - Fix non-english requests
 */
   require_once('../inc/servervars.php');
-  
+
   if(isset($_REQUEST['keyboard'])) $keyboard = $_REQUEST['keyboard']; else $keyboard = '';
   if(isset($_REQUEST['language'])) $language = $_REQUEST['language']; else $language = '';
-  
-  if($keyboard == '')
-  {
-    echo "No example for this language.";
+
+  if(empty($keyboard) || empty($language)) {
+    $data = FALSE;
+  } else {
+    require_once("renderLanguageExample.php");
+    $data = renderLanguageExample($keyboard, $language);
   }
-  else
-  {
+
+  if($data !== FALSE) {
+    echo $data;
+  } else {
+    // TODO: unify with keyboard-help-ajax.php
     $morehelp = "  <a target='KeymanWebHelp' onclick='javascript:return openALink(this)' " .
       "title='Keyboard help'" .
       "href=\"http://$site_keymanwebhelp/go?language=$language&amp;keyboard=$keyboard\">Keyboard help</a>";
     $morehelp = "  <a target='KeymanWebHelp' onclick='javascript:return openALink(this)' " .
       "title='Keyboard help'" .
       "href=\"http://$site_keymanwebhelp/go?language=$language&amp;keyboard=$keyboard\"> <img src='".cdn("img/helpIcon.png")."'> </a>";
-      
-    require_once("renderLanguageExample.php");
-   
-   renderLanguageExample($keyboard, $language, $morehelp);
-  }  
+
+    echo '<br/>No example is available, please refer to keyboard help'.$morehelp;
+  }
 ?>
