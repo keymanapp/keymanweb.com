764e39f5ec3869a3e8bc0a7574edadb4c9579b0c
MengHeng <90595388+Meng-Heng@users.noreply.github.com>
2024-10-23 14:30:12 +0700

    Apply suggestions from code review

--- resources/init-container.sh
+++ resources/init-container.sh
@@ -2,11 +2,10 @@
 
 if [[ ! $1 =~ "debug" ]]; then
     echo "---- Generating CDN ---"
-    rm -rf cdn/deploy
-    cd cdn
+    cd ../cdn
     php -d include_path=/var/www/html/_includes:. cdnrefresh.php
     cd ..
 else 
     echo "Skip Generating CDN and clean CDN cache"
-    rm -rf cdn/deploy
+    rm -rf ../cdn/deploy
 fi
